5d2d2f4e008a9bff06d62f5a978f2155baba0967
Kayla Justice <kaylarjustice@gmail.com>
2022-10-19 14:55:24 -0500

    Started .env and .env.example files.

--- server.js
+++ server.js
@@ -2,3 +2,17 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 // Import and require mysql2
 const mysql = require('mysql2');
+require('dotenv').config();
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'password',
+      database: 'DATABASE_NAME'
+    },
+    console.log(`Connected to the DATABASE_NAME database.`)
+  );
