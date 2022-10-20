b1233ff58e7c5245aadc0ef46ddb972c5742a1fd
Kayla Justice <kaylarjustice@gmail.com>
2022-10-20 09:26:55 -0500

    Building and populating schema and seeds.

--- db/schema.sql
+++ db/schema.sql
@@ -6,34 +6,31 @@ CREATE DATABASE employees;
 USE employees;
 
 CREATE TABLE department (
-  -- insert the id and name columns here --
+  -- id and name columns --
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  -- insert the title and salary columns here --
-  
+  -- title and salary columns --
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,  
   department_id INT UNSIGNED NOT NULL,
-  INDEX dep_ind (department_id),
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL, 
   role_id INT UNSIGNED NOT NULL,
-  INDEX role_ind (role_id),
-
-  -- using the role table above as a guide, create a constraint named fk_role on the foreign key of role_id, which --
-  -- references the id column of the role table, and performs a cascade delete when needed --
-
   -- constraint goes here --
+    INDEX role_ind (role_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
   manager_id INT UNSIGNED,
-  INDEX man_ind (manager_id),
-
-  -- using the role table above as a guide, create a constraint named fk_manager on the foreign key of manager_id, which 
-  -- references the id column of the employee table, and just sets NULL on a delete action
-  
-  -- constraint goes here
+  -- constraint goes here --
+    INDEX man_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL  
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -25,11 +25,11 @@ VALUES
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
+    ('Michael', 'Scott', 1, NULL),
+    ('Jim', 'Halpert', 2, 1),
+    ('Dwight', 'Schrute', 3, NULL),
+    ('Pam', 'Beesly', 4, 3),
+    ('Angela', 'Martin', 5, NULL),
+    ('Kevin', 'Malone', 6, 5),
+    ('Stanley', 'Hudson', 7, NULL),
+    ('Meredith', 'Palmer', 8, 7);
